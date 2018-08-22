8a8af93a39341fe3fe5b004556cd0c51dae63a33
Fernando Lima <fernando.lima@logicsp.com.br>
2018-08-22 07:35:39 -0300

    Arrumado o pacote controller e implementado o Docker-compose

--- template/template-server/src/main/java/logic/component/template/server/controller/EmpresaController.java
+++ template/template-server/src/main/java/logic/component/template/server/controller/EmpresaController.java
@@ -1,30 +1,52 @@
 package logic.component.template.server.controller;
 
+import io.swagger.annotations.*;
 import logic.component.template.client.EmpresaContract;
 import logic.component.template.server.service.EmpresaService;
 import logic.component.template.shared.model.Empresa;
 import logic.component.template.shared.model.Envelope;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(description = "Empresa")
 @RestController
 public class EmpresaController implements EmpresaContract {
 
     @Autowired
     EmpresaService empresaService;
 
+    @ApiOperation(value = "Busca empresa pelo id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 500, message = "Internal Server Error")})
+    @ResponseStatus(HttpStatus.OK)
     @Override
     public Envelope getByidEmpresa(@PathVariable Long id) throws Exception {
         return new Envelope().setData(empresaService.getByIdEmpresa(id));
     }
 
+    @ApiOperation(value = "Persiste uma empresa")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 500, message = "Internal Server Error")})
+    @ResponseStatus(HttpStatus.OK)
     @Override
     public Envelope insertEmpresa(@RequestBody Empresa empresa) throws Exception {
         return new Envelope().setData(empresaService.insertEmpresa(empresa));
     }
 
+    @ApiOperation(value = "Busca todas as empresas")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 500, message = "Internal Server Error")})
+    @ResponseStatus(HttpStatus.OK)
     @Override
     public Envelope getAllEmpresa() {
         return new Envelope().setData(empresaService.getAllEmpresa());
--- template/template-server/src/main/java/logic/component/template/server/controller/FuncionarioController.java
+++ template/template-server/src/main/java/logic/component/template/server/controller/FuncionarioController.java
@@ -1,30 +1,55 @@
 package logic.component.template.server.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import logic.component.template.client.FuncionarioContract;
 import logic.component.template.server.service.FuncionarioService;
 import logic.component.template.shared.model.Envelope;
 import logic.component.template.shared.model.Funcionario;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(description = "Funcionario")
 @RestController
 public class FuncionarioController implements FuncionarioContract {
 
     @Autowired
     FuncionarioService funcionarioService;
 
+    @ApiOperation(value = "Busca funcionario pelo id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 500, message = "Internal Server Error")})
+    @ResponseStatus(HttpStatus.OK)
     @Override
     public Envelope getByidFuncionario(@PathVariable Long id) throws Exception {
         return new Envelope().setData(funcionarioService.getByIdFuncionario(id));
     }
 
+    @ApiOperation(value = "Persiste um funcionario")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 500, message = "Internal Server Error")})
+    @ResponseStatus(HttpStatus.OK)
     @Override
     public Envelope insertFuncionario(@RequestBody Funcionario funcionario) throws Exception {
         return new Envelope().setData(funcionarioService.insertFuncionario(funcionario));
     }
 
+    @ApiOperation(value = "Busca todos os funcionario")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 500, message = "Internal Server Error")})
+    @ResponseStatus(HttpStatus.OK)
     @Override
     public Envelope getAllFuncionario() {
         return new Envelope().setData(funcionarioService.getAllFuncionario());
